ddff1573ddb9975eaea051aef9bcf957be20f2a6
Gabriel Magreti <gabrielmagreti@gmail.com>
2022-03-20 14:44:39 -0300

    Feat: Creating a stripe instance

--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -3,7 +3,7 @@ import Stripe from 'stripe';
 export const stripe = new Stripe(process.env.STRIPE_API_KEY, {
   apiVersion: '2020-08-27',
   appInfo: {
-    name: 'ignews',
+    name: 'Ignews',
     version: '0.1.0',
   },
 });
