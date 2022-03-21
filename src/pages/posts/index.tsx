3db90645389fd4e64186db177a21daf5af798da5
Gabriel Magreti <gabrielmagreti@gmail.com>
2022-03-21 00:44:14 -0300

    Feat: adding function to return to top after scrolling.

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -5,6 +5,7 @@ import { getPrismicClient } from '../../services/prismic';
 import styles from './styles.module.scss';
 import Prismic from '@prismicio/client';
 import { RichText } from 'prismic-dom';
+import { useEffect, useState } from 'react';
 
 type Post = {
   slug: string;
@@ -18,6 +19,30 @@ interface PostsProps {
 }
 
 export default function Posts({ posts }: PostsProps) {
+  const [belowTheTopOfTheScreen, setBelowTheTopOfTheScreen] = useState(false);
+
+  const toggleVisibility = () => {
+    if (window.scrollY >= 50) {
+      if (!belowTheTopOfTheScreen) {
+        setBelowTheTopOfTheScreen(true);
+      }
+    } else {
+      if (belowTheTopOfTheScreen) {
+        setBelowTheTopOfTheScreen(false);
+      }
+    }
+  };
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', toggleVisibility);
+  }, []);
+
   return (
     <>
       <Head>
@@ -36,6 +61,25 @@ export default function Posts({ posts }: PostsProps) {
           ))}
         </div>
       </main>
+
+      {belowTheTopOfTheScreen && (
+        <button onClick={scrollToTop} className={styles.buttonArrowTop}>
+          <svg
+            stroke="#fff"
+            fill="#fff"
+            stroke-width="0"
+            viewBox="0 0 16 16"
+            height="1.5em"
+            width="1.5em"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M8 12a.5.5 0 0 0 .5-.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 .5.5z"
+            ></path>
+          </svg>
+        </button>
+      )}
     </>
   );
 }
@@ -51,8 +95,6 @@ export const getStaticProps: GetStaticProps = async () => {
   );
 
   const posts = response.results.map((post) => {
-    console.log(post);
-
     return {
       slug: post.uid,
       title: RichText.asText(post.data.title),
